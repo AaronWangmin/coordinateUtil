1d8cc252ec6b59eb4f48aadf3f157099bc8a1290
AaronWangmin <minwang80@126.com>
2018-04-10 11:49:29 +0800

    add Testxy2BL()

--- coordinateUtil/test/com/shbd/ddap/coordinate/TestCoordTransform.java
+++ coordinateUtil/test/com/shbd/ddap/coordinate/TestCoordTransform.java
@@ -17,10 +17,29 @@ public class TestCoordTransform {
 		
 		assert (result.getX()-3418060.509 <= 0.05) && (result.getY()-680157.668 <= 0.05);
 		
-		System.out.println(result);
+		System.out.println(result);	
+		
 		
 	}
 	
-	
+	@Test
+	public void testxy2BL() {
+		
+		Ellipsoid re = ConstantHolder.ReferenceEllipsoid.BJ54;
+		double x = 3418060.509;
+		double y = 680157.668;
+		double L0 = CoordTransform.deg2Rad(120);
+		double yAdd = 500000;
+		
+		Coordinate result = CoordTransform.xy2BL(re, x, y, L0, yAdd);
+		
+		double degB = CoordTransform.rad2Deg(result.getX());
+		double degL = CoordTransform.rad2Deg(result.getY());
+		
+		assert (degB-30.8692658056 <= CoordTransform.as2Rad(0.0001)) && (degL-121.8836248944 <= CoordTransform.as2Rad(0.0001));
+		
+		System.out.println(degB + " , " + degL);
+		
+	}
 
 }
