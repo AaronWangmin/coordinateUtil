08b268d5aea2312dd0cdff704cc32c29a13c18c4
AaronWangmin <minwang80@126.com>
2018-04-10 11:46:42 +0800

    add xy2BL

--- coordinateUtil/src/com/shbd/ddap/coordinate/CoordTransform.java
+++ coordinateUtil/src/com/shbd/ddap/coordinate/CoordTransform.java
@@ -147,6 +147,71 @@ public class CoordTransform {
 	}
 	
 	/**
+	 * 高斯平面坐标 --> 大地坐标（角度单位都是弧度）
+	 * @param re
+	 * @param x
+	 * @param y
+	 * @param L0
+	 * @param addY
+	 * @return
+	 */
+	public static Coordinate xy2BL(Ellipsoid re,double x,double y,double L0,double yAdd) {
+		y = y -yAdd;
+		
+		double a = re.getA();	// 椭球长半轴
+		double e1 = re.getE1();
+		double e2 = re.getE2();
+		
+		double C1 = 1.0 + (3.0/4) * Math.pow(e1, 2) + (45.0/64) * Math.pow(e1, 4) 
+				+ (175.0/256) * Math.pow(e1, 6) + (11025.0/16384) * Math.pow(e1, 8)
+				+ (43659.0/65536)*Math.pow(e1, 10);
+		
+		double C2 = (3.0/4) * Math.pow(e1, 2) + (15.0/16) * Math.pow(e1, 4) 
+				+ (525.0/512) * Math.pow(e1, 6) + (2205.0/2048) * Math.pow(e1, 8)
+				+ (72765.0/65536)*Math.pow(e1, 10);		
+		
+		double C3 = (15.0/64) * Math.pow(e1, 4) + (105.0/256) * Math.pow(e1, 6) 
+				+ (2205.0/4096) * Math.pow(e1, 8) + (10395.0/16384)*Math.pow(e1, 10);
+		
+		double C4 = (35.0/512) * Math.pow(e1, 6) + (315.0/2048) * Math.pow(e1, 8)
+				+ (31185.0/131072)*Math.pow(e1, 10);
+		
+		double C5 = (315.0/16384) * Math.pow(e1, 8) + (3645.0/65536)*Math.pow(e1, 10);
+		
+		// 迭代计算Bf,底点纬度（横坐标y在高斯投影带中央子午线上的垂足点的纬度）
+		double Bf0 = x / (a*C1*(1-e1*e1));
+		double Bf1 = 0.0;
+		while(true) {
+			Bf1 = x / (a*C1*(1-e1*e1)) + (1.0/C1)*( (1.0/2)*C2*Math.sin(2*Bf0) 
+												  - (1.0/4)*C3*Math.sin(4*Bf0)
+				                                  + (1.0/6)*C4*Math.sin(6*Bf0)
+				                                  - (1.0/8)*C5*Math.sin(8*Bf0));
+			if(Math.abs(Bf1-Bf0) < CoordTransform.as2Rad(0.0001)) break;  // less than 0.0001 second.
+			else{
+				Bf0 = Bf1;
+			}
+		}
+		
+		// 底点纬度Bf处的子午圈曲率半径
+		double Mf = a*(1-e1*e1)/Math.sqrt(Math.pow(1-Math.pow(e1*Math.sin(Bf1), 2), 3));
+		// 底点纬度Bf处的卯酉圈曲率半径
+		double Nf = a/Math.sqrt(1-Math.pow(e1*Math.sin(Bf1), 2));
+		double t = Math.tan(Bf1);
+//		double eta = Math.pow(e2 * Math.cos(Bf1),2);
+		double eta = e2 * Math.cos(Bf1);
+	
+		double B = Bf1 - y*y*t/(2*Mf*Nf) 
+				*(1 - y*y*(5 + eta*eta + 3*t*t - 9*eta*eta*t*t)/(12*Nf*Nf) 
+				    + Math.pow(y, 4)*(61+90*t*t+45*t*t)/(360*Math.pow(Nf, 4)) );
+		
+		double L = L0 + y/(Nf*Math.cos(Bf1))
+				*(1 - y*y*(1 + eta*eta + 2*t*t)/(6*Nf*Nf) 
+				    + Math.pow(y, 4)*(5 + 6*eta*eta + 28*t*t + 8*eta*eta*t*t + 24*Math.pow(t, 4))/(120*Math.pow(Nf, 4)));
+		
+		return new Coordinate(re,CoordinateType.GEODETIC,B,L,0);
+	}
+	
+	/**
 	 * 空间直角坐标--> 站心坐标（NEU）
 	 * @param originPoint：站心点的空间直角坐标,(XYZ)
 	 * @param targetPoint：目标点的空间直角坐标,(XYZ)
