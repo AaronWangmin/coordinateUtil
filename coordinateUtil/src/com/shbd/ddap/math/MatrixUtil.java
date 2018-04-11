f82951644472fa9180bda25c6d9b6b2f82cf4ebf
AaronWangmin <minwang80@126.com>
2018-04-11 14:13:43 +0800

    add MatriUtil

---
+++ coordinateUtil/src/com/shbd/ddap/math/MatrixUtil.java
@@ -0,0 +1,23 @@
+package com.shbd.ddap.math;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+
+public class MatrixUtil {
+	
+	// 构造给定大小的单位矩阵
+	public static Array2DRowRealMatrix eye(int size) {
+		Array2DRowRealMatrix result = new Array2DRowRealMatrix(size,size);
+		for(int i=0;i<size;i++) {			
+			result.setEntry(i, i, 1.0);
+		}
+		return result;		
+	}
+	
+	// 计算给定矩阵的逆矩阵
+	public static RealMatrix inverseMatrix(RealMatrix A) {
+		RealMatrix result = new LUDecomposition(A).getSolver().getInverse();		
+		return result;		
+	}
+}
