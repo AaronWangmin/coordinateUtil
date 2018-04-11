04c5b4b5f5a137347d97710addfd8065243f6eda
AaronWangmin <minwang80@126.com>
2018-04-11 14:17:00 +0800

    update the class of TestCoordTransform

--- coordinateUtil/test/com/shbd/ddap/coordinate/TestCoordTransform.java
+++ coordinateUtil/test/com/shbd/ddap/coordinate/TestCoordTransform.java
@@ -1,5 +1,8 @@
 package com.shbd.ddap.coordinate;
 
+import java.util.ArrayList;
+
+import org.apache.commons.math3.linear.RealVector;
 import org.junit.Test;
 
 public class TestCoordTransform {
@@ -39,6 +42,33 @@ public class TestCoordTransform {
 		assert (degB-30.8692658056 <= CoordTransform.as2Rad(0.0001)) && (degL-121.8836248944 <= CoordTransform.as2Rad(0.0001));
 		
 		System.out.println(degB + " , " + degL);
+		System.out.println(result);
+		
+	}
+	
+	@Test
+	public void testCalculateServenParam() {
+		
+		
+		ArrayList<Coordinate> sources = new ArrayList<Coordinate>();
+		Coordinate source1 = new Coordinate(-2858951.7086, 4660391.5147, 3273543.0373);
+		Coordinate source2 = new Coordinate(-2888608.4197, 4655861.6904, 3254028.1766);
+		Coordinate source3 = new Coordinate(-2849958.9961, 4688645.1877, 3241078.5745);
+		sources.add(source1);
+		sources.add(source2);
+		sources.add(source3);
+		
+		ArrayList<Coordinate> targets = new ArrayList<Coordinate>();
+		Coordinate target1 = new Coordinate(-17199.2926, 5744.6724, 0);
+		Coordinate target2 = new Coordinate(-39909.6566, 33405.4226, 0);
+		Coordinate target3 = new Coordinate(-55021.4263, -16673.3169, 0);
+		targets.add(target1);
+		targets.add(target2);
+		targets.add(target3);
+		
+		RealVector result = CoordTransform.calculateServenParam(sources, targets);
+		System.out.println(result.toString());
+		
 		
 	}
 
