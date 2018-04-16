7895e39b01e7ffac5f4050cec9c0600c5972fa7e
AaronWangmin <minwang80@126.com>
2018-04-16 14:56:10 +0800

    update testCalculateFourParam()

--- coordinateUtil/test/com/aaron/survey/coordinate/TestCoordTransform.java
+++ coordinateUtil/test/com/aaron/survey/coordinate/TestCoordTransform.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.commons.math3.analysis.function.Abs;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.aaron.survey.ConstantHolder;
@@ -13,7 +14,6 @@ import com.aaron.survey.angle.AngleUtil;
 import com.aaron.survey.coordinate.CoordinateUtil;
 import com.aaron.survey.coordinate.Coordinate;
 import com.aaron.survey.coordinate.Ellipsoid;
-import com.aaron.survey.matrix.MatrixUtil;
 
 public class TestCoordTransform {
 
@@ -101,5 +101,73 @@ public class TestCoordTransform {
 		System.out.println("diff----------------");
 		System.out.println(diff.toString());
 		pa.showAdjustResult();
+		
+		System.out.println(pa.calculateX().toString());
 	}
+	
+	@Test
+	public void testECEF2ECEFByServenParam() {
+		Coordinate source = new Coordinate(-2079412.5535, 5512450.8800, 2879771.2119);
+		
+		ArrayRealVector servenParam = new ArrayRealVector(
+				new double[] {280.0836790372,57.4950097981,116.6476522069,
+							  0.0000006305,
+							  0.0000142449,0.0000171129,-0.0000228361});
+		
+		Coordinate actual = CoordinateUtil.ECEF2ECEFByServenParam(source,
+				servenParam,ConstantHolder.ReferenceEllipsoid.BJ54);
+		
+		System.out.println(actual.toString());
+		
+		Coordinate expected = new Coordinate(-2079308.984,5512505.3689,2879775.4919);
+		System.out.println("X坐标误差: " + (actual.getX() - expected.getX()));
+		System.out.println("Y坐标误差: " + (actual.getY() - expected.getY()));
+		System.out.println("Z坐标误差: " + (actual.getZ() - expected.getZ()));
+		
+		Assert.assertTrue("X坐标误差大于0.1米", Math.abs(actual.getX() - expected.getX())<0.1);
+		Assert.assertTrue("Y坐标误差大于0.1米", Math.abs(actual.getY() - expected.getY())<0.1);
+		Assert.assertTrue("Z坐标误差大于0.1米", Math.abs(actual.getZ() - expected.getZ())<0.1);
+	}
+	
+	@Test
+	public void testCalculateFourParam() {
+		ArrayList<Coordinate> sources = new ArrayList<Coordinate>();
+		Coordinate source1 = new Coordinate(3348724.128, 526679.306, 0);
+		Coordinate source2 = new Coordinate(3324702.854, 522575.273, 0);
+		sources.add(source1);
+		sources.add(source2);
+		
+		ArrayList<Coordinate> targets = new ArrayList<Coordinate>();
+		Coordinate target1 = new Coordinate(81281.89, 91322.285, 0);
+		Coordinate target2 = new Coordinate(57266.11, 87186.224, 0);
+		targets.add(target1);
+		targets.add(target2);
+		
+		ParamAdjust fourParam = CoordinateUtil.calculateFourParam(sources, targets);
+		
+		// 获取程序计算所得4参数
+				RealVector X = fourParam.calculateX();
+				
+		// 将一个旋转参数的单位设置为：秒		
+		X.setEntry(3, AngleUtil.rad2As(X.getEntry(3)));
+		
+		X.setEntry(2, 1+X.getEntry(2));
+		
+		// 4参数的对比值（已知值）
+		ArrayRealVector ref_X= new ArrayRealVector(
+				new double[] {-3266736.944339,-439821.976053,
+							  0.999999998447,
+						      AngleUtil.rad2As(0.0013334707)});	// 秒
+		
+		// 若最大值小于 0.001，则成功
+		RealVector diff = (X.add(ref_X.mapMultiplyToSelf(-1))).map(new Abs());
+		assert (diff.getMaxValue() < Math.pow(10, -3));
+		
+		System.out.println("/n---------------------4参数----------------------");
+		System.out.println("diff----------------");
+		System.out.println(diff.toString());
+		
+		fourParam.showAdjustResult();
+	}
+	
 }
